57a779dd6b9e7e0d0ffed4c8c94e3a3fe60927a0
fsioni <fares.sioni@gmail.com>
2022-05-24 21:23:14 +0200

    Dividing main.js with MyWindow.js for better project files structure

---
+++ src/MyWindow.js
@@ -0,0 +1,81 @@
+const windowStateKeeper = require("electron-window-state");
+const {BrowserWindow, app, ipcMain, dialog} = require("electron");
+const path = require("path");
+
+class MyWindow {
+    win = null;
+    mainWindowState = null;
+
+    async openWindowSelectTextFile() {
+        const {canceled, filePaths} = await dialog.showOpenDialog({
+            filters: [{name: 'Text files', extensions: ['txt']}],
+            properties: [
+                'openFile']
+
+        })
+        if (canceled) {
+            return null;
+        } else {
+            return filePaths[0]
+        }
+    }
+
+    async openWindowSelectSoundFile() {
+        const {canceled, filePaths} = await dialog.showOpenDialog({
+            filters: [{name: 'Sound files', extensions: ['mp3', 'wav']}],
+            properties: [
+                'openFile']
+
+        })
+        if (canceled) {
+            return null;
+        } else {
+            return filePaths[0]
+        }
+    }
+
+    createWindow = () => {
+        console.log("createWindow");
+        this.mainWindowState = windowStateKeeper({
+            defaultWidth: 1000,
+            defaultHeight: 800
+        })
+
+        this.win = new BrowserWindow({
+            x: this.mainWindowState.x,
+            y: this.mainWindowState.y,
+            width: this.mainWindowState.width,
+            height: this.mainWindowState.height,
+            resizable: false,
+            title: "Stream Timer",
+            autoHideMenuBar: true,
+            icon: path.join(__dirname, '../public/icon.png'),
+            webPreferences: {
+                preload: path.join(__dirname, 'preload.js'),
+                nodeIntegration: false,
+                contextIsolation: true
+            }
+        });
+
+        this.win.webContents.openDevTools();
+
+        this.win.loadFile(path.join(__dirname, '../public/index.html'));
+    }
+
+    initWindow = () => {
+        this.mainWindowState.manage(this.win);
+
+        app.on('window-all-closed', () => {
+            if (process.platform !== 'darwin') app.quit()
+        });
+
+        app.on('activate', () => {
+            if (BrowserWindow.getAllWindows().length === 0) this.createWindow()
+        });
+
+        ipcMain.handle('open-select-output-file-window', this.openWindowSelectTextFile);
+        ipcMain.handle('open-select-sound-window', this.openWindowSelectSoundFile);
+    }
+}
+
+module.exports = MyWindow;
